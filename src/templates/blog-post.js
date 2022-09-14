762efdc9b620043d8050d6e25b93c2f7dc82ec08
Willian Lauber <willianlauber@jalasoft.com>
2022-09-14 16:32:31 -0400

    Refactor: code standards

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-export default blog =  ({data}) => {
+const blog =  ({data}) => {
     const post = data.markdownRemark;
     return (
         <Layout>
             <div>
                 <h1>{post.frontmatter.tittle}</h1>
-                <div dangerouslySetInnerHTML={{___html: post.html}}/>
+                <div dangerouslySetInnerHTML={{__html: post.html}}/>
             </div>
         </Layout>
         )
 }
-
+export default blog;
 export const query = graphql`
 query($slug: String!){
     markdownRemark( fields: { slug: { eq: $slug}}){
